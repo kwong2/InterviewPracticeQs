99d38e65fa16321f82f4c20f07f29134bb3cd9c5
Kevin Wong <k.wong4@yahoo.com>
2017-07-03 17:42:39 -0400

    Changed file name from Permutation to CheckPermutation

---
+++ IsUnique.js
@@ -0,0 +1,10 @@
+// # Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+// # cannot use additional data structures?
+
+// Use the split method
+
+function veryunique(string){
+	console.log(string.split(""));
+}
+
+veryunique("abcdef")
