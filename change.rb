cc0d715d8cf187fcc34ec7ee206e94f08002be18
Kevin Wong <k.wong4@yahoo.com>
2017-08-01 14:43:47 -0400

    starting change answer

---
+++ change.rb
@@ -0,0 +1,17 @@
+# make a max function that takes highest denomination under a number
+# make change function that subtracts largest coin until change has been issued.
+
+def largestcoin(amount, coins)
+	best_coin = coins[0]
+	for coin in coins
+		if (coin < amount) && (coin > best_coin)
+			best_coin = coin
+		end
+	end
+	best_coin
+end
+
+def make_change(amount)
+	possible_coins = [1,5,10,15,25]
+	
+end
