fd9188a2f4c82da0b5b92d3c0620c253fb357ef7
Kevin Wong <k.wong4@yahoo.com>
2017-08-05 16:09:58 -0400

    error message

--- change2.rb
+++ change2.rb
@@ -1,38 +1,41 @@
-# def largest_coin(amount,coins)
+def largest_coin(amount,coins)
+	best_coin = coins[0]
+	for coin in coins
+		if (coin <= amount) && (coin > best_coin)
+			best_coin = coin
+		end
+	end
+	p best_coin
+end
+
+# def largest_coin(amount, coins)
 # 	i = 0
-# 	best_coin = coins[i]
-# 	for coin in coins
-# 		if (coin <= amount) && (coin > best_coin)
-# 			best_coin = coin
+# 	while i < coins.length
+# 		best_coin = coins[i]
+# 		if (best_coin <= amount) && (best_coin > coins[i])
+# 			best_coin
+# 		else 
+# 			i += 1
 # 		end
 # 	end
 # 	p best_coin
 # end
 
-def largest_coin(amount, coins)
-	i = 0
-	while i < coins.length-1
-		best_coin = coins[i]
-		if (best_coin <= amount) && (best_coin > coins[i])
-			best_coin
-		else 
-			i += 1
-		end
-	end
-	best_coin
-end
-
-# largest_coin(75,[30,50,45,15])
+# largest_coin(75,[25,50,10,15])
 
 def change(amount)
-	denoms = [1,5,10,25,50,100].reverse
+	denoms = [5,1,20,25,75,50,30,100]
 	arr = []
 	until amount <= 0
-		chosen_one = largest_coin(amount,denoms)
+		chosen_one = largest_coin(amount,denoms.sort)
 		arr << chosen_one
 		amount -= chosen_one
 	end
-	p arr
+	if arr == []
+		p "You must be joking right?"
+	else
+		p arr
+	end
 end
 
-change(30)
+change(77)
