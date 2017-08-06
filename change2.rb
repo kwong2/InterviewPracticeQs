8739319f2f47941c96dac1b0b6463c834e4fa4b6
Kevin Wong <k.wong4@yahoo.com>
2017-08-06 15:22:04 -0400

    slight refractor

--- change2.rb
+++ change2.rb
@@ -10,32 +10,20 @@ def largest_coin_finder(amount,coins)
 	largest_coin
 end
 
-# def smallest_coin_finder(amount, coins)
-# 	smallest_coin = coins[0]
-# 	for coin in coins 
-# 		if coin < smallest_coin
-# 			smallest_coin = coin
-# 		end
-# 	end
-# 	smallest_coin
-# end
-
-# largest_coin(75,[80,90])
-
 def change(amount)
-	coins = [80,90]
+	coins = [1,5,10,25,50]
 	arr = []
 	largest_coin = largest_coin_finder(amount, coins.sort)
 	if amount < largest_coin
-		puts "There are no coins to make this amount possible."
+		puts "You don't have the coins needed to make the amount."
 	else
 		until amount <= 0
 			chosen_one = largest_coin_finder(amount,coins.sort)
 			arr << chosen_one
-			amount -= chosen_one
+			amount -= chosen_one	 
 		end
+		puts "Your change is: #{arr}"
 	end
-	puts "Your change is: #{arr}" if arr.count >= 1
 end
 
-change(280)
+change(-240)
