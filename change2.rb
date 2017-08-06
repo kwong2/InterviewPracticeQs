09d56d293abcf506bd5061cf55c77e2fc807d5d2
Kevin Wong <k.wong4@yahoo.com>
2017-08-05 23:29:57 -0400

    added put statements

--- change2.rb
+++ change2.rb
@@ -5,7 +5,7 @@ def largest_coin(amount,coins)
 			best_coin = coin
 		end
 	end
-	p best_coin
+	best_coin
 end
 
 # def largest_coin(amount, coins)
@@ -24,18 +24,15 @@ end
 # largest_coin(75,[25,50,10,15])
 
 def change(amount)
-	denoms = [5,1,20,25,75,50,30,100]
+	denoms = [1, 5, 10, 25]
 	arr = []
 	until amount <= 0
 		chosen_one = largest_coin(amount,denoms.sort)
 		arr << chosen_one
 		amount -= chosen_one
 	end
-	if arr == []
-		p "You must be joking right?"
-	else
-		p arr
-	end
+	puts "Your change is: #{arr}" if arr.count >= 1
+	puts "You must be joking right?" if arr == []
 end
 
-change(77)
+change(43)
